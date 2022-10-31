0c746f5b67db0638547203bb74886d48c58eb2b0
polinakoeva <polina.koeva@gmail.com>
2022-10-31 13:10:09 +0100

    Separate logic into different files

---
+++ src/js/gameObjects.js
@@ -0,0 +1,12 @@
+//тук ще дефинираме всички DOM-обекти
+
+function initGameObject() {
+
+    const startScreen = document.querySelector('.start-screen');
+    const gameScreen = document.querySelector('.game-screen');
+
+    return {
+        startScreen,
+        gameScreen
+    };
+}
---
+++ src/js/gameState.js
@@ -0,0 +1,7 @@
+// тук ще държим цялата информация за текущото състояние на нашата игра
+function initState() {
+    const state =  {
+        player: 'Pesho'
+    }
+}
+return state;
