50e54148bc8180e1cd3e419dd4a985dfc0586253
Polina Koeva <polina.koeva@gmail.com>
2022-11-08 13:18:31 +0100

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -9,13 +9,13 @@ const availableKeys = [
     'Space',
 ];
 
-document.addEventListener('keydown', (e) => { //keydown използваме, коагто искаме да натиснем и задържим, за да се мести героя ни постоянно
+document.addEventListener('keydown', (e) => { 
     if (availableKeys.includes(e.code)) {
         state.keys[e.code] = true;
     }
 });
-document.addEventListener('keyup', (e) => { //keyup - когато си вдигнем пръста от бутона
-    state.keys[e.code] = false; // за съответния key, който е бил натиснат
+document.addEventListener('keyup', (e) => { 
+    state.keys[e.code] = false; 
 });
 
 game.startScreen.addEventListener('click', (e) => {
